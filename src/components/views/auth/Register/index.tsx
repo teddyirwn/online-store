d2e28bb3c2a20201a5ea97e8a2bdf9d0e78de821
teddyirwn <tdyirawan0@gmail.com>
2023-12-30 16:22:44 +0700

    feat: register

---
+++ src/components/views/auth/Register/index.tsx
@@ -0,0 +1,87 @@
+import Link from 'next/link';
+import styles from './Register.module.scss';
+import { push } from 'firebase/database';
+import { useRouter } from 'next/router';
+import { FormEvent, useState } from 'react';
+
+const RegisterView = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const { push } = useRouter();
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+        setError('');
+        const form = e.target as HTMLFormElement
+        const data = {
+            email: form.email.value,
+            fullname: form.fullname.value,
+            phone: form.phone.value,
+            password: form.password.value,
+        }
+
+        const result = await fetch('/api/user/register', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (result.status === 200) {
+            form.reset();
+            setIsLoading(false);
+            push('/auth/login');
+        } else {
+            setIsLoading(false)
+            setError('email  is already registered')
+        }
+
+    }
+
+    return (
+        <div className={styles.register}>
+            <h1 className={styles.register__title}>Register</h1>
+            {error && <p className={styles.register__error}>{error}</p>}
+            <div className={styles.register__form}>
+                <form onSubmit={handleSubmit}>
+                    <div className={styles.register__item}>
+                        <label htmlFor="fullname">Fullname</label>
+                        <input
+                            name='fullname'
+                            id='fullname'
+                            type="text"
+                            className={styles.register__item__input} />
+                    </div>
+                    <div className={styles.register__item}>
+                        <label htmlFor="email">Email</label>
+                        <input name='email'
+                            id='email'
+                            type="email"
+                            className={styles.register__item__input} />
+                    </div>
+                    <div className={styles.register__item}>
+                        <label htmlFor="phone">Phone</label>
+                        <input name='phone'
+                            id='phone'
+                            type="text"
+                            className={styles.register__item__input} />
+                    </div>
+                    <div className={styles.register__item}>
+                        <label htmlFor="password">Password</label>
+                        <input name='password'
+                            id='password'
+                            type="password"
+                            className={styles.register__item__input} />
+                    </div>
+                    <button type='submit' className={styles.register__button}>{isLoading ? 'Loading...' : 'Register'}</button>
+
+                </form>
+                <p className={styles.register__link}>Have an account? Sign in <Link href="/auth/login">here</Link> </p>
+            </div>
+        </div>
+    )
+}
+
+export default RegisterView;
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,12 @@
+import RegisterView from "@/components/views/auth/Register";
+
+const RegisterPage = () => {
+    return (
+        <>
+            <RegisterView />
+        </>
+    );
+}
+
+export default RegisterPage;
+
